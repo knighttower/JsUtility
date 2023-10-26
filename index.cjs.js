7d1eab85313fba1d5b8e02b01b048bfe4da32f80
Knighttower <knighttower+git@outlook.com>
2023-10-26 16:08:00 -0400

    minor fixes --VERIFIED

--- index.cjs.js
+++ index.cjs.js
@@ -3,7 +3,26 @@ const DomObserver = require('./src/DomObserver.js');
 // Default Module from: ElementHelper
 const ElementHelper = require('./src/ElementHelper.js');
 // Single Modules and Aliases from: PowerHelpers
-const { PowerHelper, addQuotes, cleanStr, convertKeysToSymbols, findAndReplaceInArray, findNested, fixQuotes, getArrObjFromString, getDirectivesFromString, getMatchBlock, getChunks, getMatchInBetween, removeQuotes, startAndEndWith, setExpString, setLookUpExp, setWildCardString, wildCardStringSearch } = require('./src/PowerHelpers.js');
+const {
+    PowerHelper,
+    addQuotes,
+    cleanStr,
+    convertKeysToSymbols,
+    findAndReplaceInArray,
+    findNested,
+    fixQuotes,
+    getArrObjFromString,
+    getDirectivesFromString,
+    getMatchBlock,
+    getChunks,
+    getMatchInBetween,
+    removeQuotes,
+    startAndEndWith,
+    setExpString,
+    setLookUpExp,
+    setWildCardString,
+    wildCardStringSearch,
+} = require('./src/PowerHelpers.js');
 // Default Module from: PowerHelpers
 const powerHelper = require('./src/PowerHelpers.js');
 // Default Module from: ProxyHelper
@@ -11,23 +30,82 @@ const ProxyHelper = require('./src/ProxyHelper.js');
 // Default Module from: UrlHelper
 const UrlHelper = require('./src/UrlHelper.js');
 // Single Modules and Aliases from: Utility
-const { utils, utility, convertToBool, convertToNumber, currencyToDecimal, dateFormat, decimalToCurrency, emptyOrValue, formatPhoneNumber, getDynamicId, getRandomId, getGoogleMapsAddress, includes, isEmpty, isNumber, instanceOf, openGoogleMapsAddress, proxyObject, selectElement, toCurrency, toDollarString, typeOf, validateEmail, validatePhone } = require('./src/Utility.js');
+const {
+    utils,
+    utility,
+    convertToBool,
+    convertToNumber,
+    currencyToDecimal,
+    dateFormat,
+    decimalToCurrency,
+    emptyOrValue,
+    formatPhoneNumber,
+    getDynamicId,
+    getRandomId,
+    getGoogleMapsAddress,
+    includes,
+    isEmpty,
+    isNumber,
+    instanceOf,
+    openGoogleMapsAddress,
+    proxyObject,
+    selectElement,
+    toCurrency,
+    toDollarString,
+    typeOf,
+    validateEmail,
+    validatePhone,
+} = require('./src/Utility.js');
 // Default Module from: Utility
 const Utility = require('./src/Utility.js');
 
-// Default Module from: DomObserver
-module.exports = { DomObserver };
-// Default Module from: ElementHelper
-module.exports = { ElementHelper };
-// Single Modules and Aliases from: PowerHelpers
-module.exports = { PowerHelper, addQuotes, cleanStr, convertKeysToSymbols, findAndReplaceInArray, findNested, fixQuotes, getArrObjFromString, getDirectivesFromString, getMatchBlock, getChunks, getMatchInBetween, removeQuotes, startAndEndWith, setExpString, setLookUpExp, setWildCardString, wildCardStringSearch };
-// Default Module from: PowerHelpers
-module.exports = { powerHelper };
-// Default Module from: ProxyHelper
-module.exports = { ProxyHelper };
-// Default Module from: UrlHelper
-module.exports = { UrlHelper };
-// Single Modules and Aliases from: Utility
-module.exports = { utils, utility, convertToBool, convertToNumber, currencyToDecimal, dateFormat, decimalToCurrency, emptyOrValue, formatPhoneNumber, getDynamicId, getRandomId, getGoogleMapsAddress, includes, isEmpty, isNumber, instanceOf, openGoogleMapsAddress, proxyObject, selectElement, toCurrency, toDollarString, typeOf, validateEmail, validatePhone };
-// Default Module from: Utility
-module.exports = { Utility };
+module.exports = {
+    DomObserver,
+    ElementHelper,
+    PowerHelper,
+    addQuotes,
+    cleanStr,
+    convertKeysToSymbols,
+    findAndReplaceInArray,
+    findNested,
+    fixQuotes,
+    getArrObjFromString,
+    getDirectivesFromString,
+    getMatchBlock,
+    getChunks,
+    getMatchInBetween,
+    removeQuotes,
+    startAndEndWith,
+    setExpString,
+    setLookUpExp,
+    setWildCardString,
+    wildCardStringSearch,
+    powerHelper,
+    ProxyHelper,
+    UrlHelper,
+    utils,
+    utility,
+    convertToBool,
+    convertToNumber,
+    currencyToDecimal,
+    dateFormat,
+    decimalToCurrency,
+    emptyOrValue,
+    formatPhoneNumber,
+    getDynamicId,
+    getRandomId,
+    getGoogleMapsAddress,
+    includes,
+    isEmpty,
+    isNumber,
+    instanceOf,
+    openGoogleMapsAddress,
+    proxyObject,
+    selectElement,
+    toCurrency,
+    toDollarString,
+    typeOf,
+    validateEmail,
+    validatePhone,
+    Utility,
+};
