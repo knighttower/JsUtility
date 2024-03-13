7281351a481719a2dfe83edafdab4bb4b4545e9e
Knighttower <knighttower+git@outlook.com>
2024-03-13 16:48:47 -0400

    WIP --VERIFIED

--- build.cjs
+++ build.cjs
@@ -8,9 +8,19 @@ const rollupConfig = `${workingDir}/packages/utility/nodeUtils/rollup.config.cjs
 
 // runCommand('ncu -u && npm i');
 // build TypeCheck
-runCommand(`cd ./packages/type-check \
-&& npx webpack --mode production --config ${webpackConfig}
-`);
+runCommand(
+    `
+cd ./packages/type-check \
+&& npx webpack --mode production --config "${webpackConfig}" \
+&& npx rollup -c "${rollupConfig}" \
+&& node "${workingDir}/packages/utility/nodeUtils/BuildExports.js" --file ./dist/cjs/TypeCheck.cjs --type=cjs \
+&& node "${workingDir}/packages/utility/nodeUtils/BuildExports.js" --file ./dist/esm/TypeCheck.js --type=esm \
+&& node "${workingDir}/packages/utility/nodeUtils/BumpVersion.cjs" --exe \
+&& eslint -c "${workingDir}/.eslintrc.json" --fix ./src --ext .js,.cjs,.mjs
+&& prettier --config "${workingDir}/.prettierrc.json" --write ./index.js \
+&& prettier --config "${workingDir}/.prettierrc.json" --write ./index.cjs
+`,
+);
 // runCommand(`npx tsc -p "${workingDir}/tsconfig.json"`);
 // runCommand(`npx rollup -c "${workingDir}/.build/rollup.config.cjs"`);
 // runCommand(`npx webpack --config "${workingDir}/.build/webpack.config.cjs"`);
