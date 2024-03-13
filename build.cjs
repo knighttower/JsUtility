b6ef3bdb42ff39666639425d4058ecb742992174
Knighttower <knighttower+git@outlook.com>
2024-03-13 15:27:26 -0400

    WIP --VERIFIED

---
+++ build.cjs
@@ -0,0 +1,11 @@
+const { runCommand } = require('./packages/utility');
+// const bumpVersion = require('@knighttower/js-utility-functions/nodeUtils/BumpVersion.cjs');
+const workingDir = process.cwd();
+
+
+
+// runCommand(`npx tsc -p "${workingDir}/tsconfig.json"`);
+// runCommand(`npx rollup -c "${workingDir}/.build/rollup.config.cjs"`);
+// runCommand(`npx webpack --config "${workingDir}/.build/webpack.config.cjs"`);
+// runCommand(`node "${workingDir}/.build/minify.cjs"`);
+// bumpVersion.exe();
